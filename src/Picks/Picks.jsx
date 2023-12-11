21dcd21bcb10ba32244fdf030259ae195e8ef209
sangeng7 <sangeng7@gmail.com>
2023-12-11 18:19:28 +0545

    Picks & Trending Responsive done!

--- src/Picks/Picks.jsx
+++ src/Picks/Picks.jsx
@@ -11,7 +11,7 @@ function Picks() {
                         <p>Abundantly creeping saw forth spirit can made appear fourth us.</p>
                     </div>
                     <div className="row g-4">
-                        <div className="col-md-6">
+                        <div className="col-lg-6 col-md-12">
                             <div className="picks-left">
                                 <div className="picks-left-img">
                                     <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/8.jpg" alt="" />
@@ -25,35 +25,37 @@ function Picks() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-lg-6 col-md-12">
                             <div className="picks-right">
-                                <div className="row g-4 align-items-center">
-                                    <div className="col-md-7">
+                                <div className="row g-4 align-items-center mb-md-4 mb-sm-5">
+                                    <div className="col-md-7 col-sm-7">
                                         <div className="picks-right-text">
                                             <a href="">Tours & Travel</a>
                                             <h4 className='mb-3 mt-2'>Light that hath itself god grass herb dark sea on the hath dowe</h4>
                                             <p>Said spirit evening above good twes at god midst deep a wherein very made he seas male very broug sad forth saying right.</p>
                                         </div>
                                     </div>
-                                    <div className="col-md-5">
+                                    <div className="col-md-5 col-sm-5">
                                         <div className="picks-right-img">
                                             <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/9.jpg" alt="" />
                                         </div>
                                     </div>
-                                    <div className="col-md-7 order-md-2">
+                                </div>
+                                <div className="row g-4 align-items-center">
+                                    <div className="col-md-7 col-sm-7 order-md-2 order-sm-2">
                                         <div className="picks-right-text">
                                             <a href="">Tours & Travel</a>
                                             <h4 className='mb-3 mt-2'>Light that hath itself god grass herb dark sea on the hath dowe</h4>
                                             <p>Said spirit evening above good twes at god midst deep a wherein very made he seas male very broug sad forth saying right.</p>
                                         </div>
                                     </div>
-                                    <div className="col-md-5 order-md-1">
+                                    <div className="col-md-5 col-sm-5 order-md-1 order-sm-1">
                                         <div className="picks-right-img">
                                             <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/10.jpg" alt="" />
                                         </div>
                                     </div>
-                                    
                                 </div>
+
                             </div>
                         </div>
                     </div>
