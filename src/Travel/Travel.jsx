8eeb7c6f4a23ea42f3e3e4a163cf52442c8eb79a
sangeng7 <sangeng7@gmail.com>
2023-12-11 14:30:46 +0545

    TravelResponsive Done!

--- src/Travel/Travel.jsx
+++ src/Travel/Travel.jsx
@@ -11,13 +11,13 @@ function Travel() {
                         <p>Abundantly creeping saw forth spirit can made appear fourth us.</p>
                     </div>
                     <div className="row">
-                        <div className="col-md-7">
+                        <div className="col-lg-7 col-md-12">
                             <div className="travel-left">
                                 <div className="travel-left-img">
                                     <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/11.jpg" alt="" />
                                 </div>
                                 <div className="travel-left-text">
-                                    <div className="">
+                                    <div className="sub-heading">
                                         <a className='' href="">shoes / <span className='currentDate'>March 15, 2019</span></a>
                                     </div>
                                     <h3 className='mt-2 mb-3'> <a href="">Created face stars sixth forth fow Earth firmament meat</a></h3>
@@ -25,15 +25,15 @@ function Travel() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-5">
+                        <div className="col-lg-5 col-md-12">
                             <div className="travel-right">
-                                <div className="row g-4">
-                                    <div className="col-md-5 pe-md-0">
+                                <div className="row mb-lg-4 mb-md-4">
+                                    <div className="col-md-5 col-sm-5  pe-md-0 pe-sm-0">
                                         <div className="travel-right-img">
                                             <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/12.jpg" alt="" />
                                         </div>
                                     </div>
-                                    <div className="col-md-7 ps-md-0">
+                                    <div className="col-md-7 col-sm-7  ps-md-0 ps-sm-0">
                                         <div className="travel-right-text p-3">
                                             <div className="sub-heading">
                                                 <a href="">Travel</a>
@@ -42,12 +42,14 @@ function Travel() {
                                             <p><a href="">Like Comment Share</a></p>
                                         </div>
                                     </div>
-                                    <div className="col-md-5 pe-md-0">
+                                </div>
+                                <div className="row mb-lg-4 mb-md-4">
+                                    <div className="col-md-5 col-sm-5  pe-md-0 pe-sm-0">
                                         <div className="travel-right-img">
                                             <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/13.jpg" alt="" />
                                         </div>
                                     </div>
-                                    <div className="col-md-7 ps-md-0">
+                                    <div className="col-md-7 col-sm-7  ps-md-0 ps-sm-0">
                                         <div className="travel-right-text p-3">
                                             <div className="sub-heading">
                                                 <a href="">Travel</a>
@@ -56,12 +58,14 @@ function Travel() {
                                             <p><a href="">Like Comment Share</a></p>
                                         </div>
                                     </div>
-                                    <div className="col-md-5 pe-md-0">
+                                </div>
+                                <div className="row mb-lg-4 mb-md-4">
+                                    <div className="col-md-5 col-sm-5  pe-md-0 pe-sm-0">
                                         <div className="travel-right-img">
                                             <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/14.jpg" alt="" />
                                         </div>
                                     </div>
-                                    <div className="col-md-7 ps-md-0">
+                                    <div className="col-md-7 col-sm-7  ps-md-0 ps-sm-0">
                                         <div className="travel-right-text p-3">
                                             <div className="sub-heading">
                                                 <a href="">Travel</a>
