74a94298d2f4c3b6e582c980f8acd3a393f23ba4
sangeng7 <sangeng7@gmail.com>
2023-12-12 14:55:47 +0545

    mapping

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -1,128 +1,137 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
 import './cards.scss'
 
 function Cards() {
+    const [News, setNews] = useState([])
+    useEffect(() => {
+        axios.get('Data/news.json')
+            .then(res => {
+                console.log(res.data);
+                setNews(res.data)
+            });
+    }, [])
     return (
         <>
             <div className="cards px-3">
                 <div className="cards-top">
                     <div className="row g-4">
-                        <div className="col-lg-5 col-md-6 col-xl-5">
-                            <div className="cards-top-left">
-                                <div className="cards-top-left-img">
-                                    <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/1.jpg" alt="" />
-                                </div>
-                                <div className="cards-top-left-text">
-                                    <div className="sub-heading">
-                                        <a className='' href="">TOURS & TRAVELS</a>
+                        {News.slice(0, 1).map((a) => (
+                            <div className="col-lg-5 col-md-6 col-xl-5">
+                                <div className="cards-top-left">
+                                    <div className="cards-top-left-img">
+                                        {/* <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/1.jpg" alt="" /> */}
+                                        <img className='w-100' src={a.img} alt="" />
                                     </div>
-                                    <h3 className='mt-2 mb-3'><a href="">Created face stars sixth forth fow Earth firmament meat</a></h3>
-                                    <p><span className='currentDate'>March 12 , 2019</span><span className='text-dark'> .By Alen Mark</span></p>
-                                </div>
+                                    <div className="cards-top-left-text">
+                                        <div className="sub-heading">
+                                            <a className='' href="">{a.title}</a>
+                                        </div>
+                                        <h3 className='mt-2 mb-3'><a href="">{a.disc}</a></h3>
+                                        <p><span className='currentDate'>March 12 , 2019</span><span className=''> .By Alen Mark</span></p>
+                                    </div>
 
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-md-6 col-xl-4">
-                            <div className="cards-top-mid">
-                                <div className="cards-top-mid-img">
-                                    <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/2.jpg" alt="" />
                                 </div>
-                                <div className="cards-top-mid-text">
-                                    <div className="sub-heading">
-                                        <a href="">TOURS & TRAVELS</a>
+                            </div>
+                        ))}
+                        {News.slice(1, 2).map((a) => (
+                            <div className="col-lg-4 col-md-6 col-xl-4">
+                                <div className="cards-top-mid">
+                                    <div className="cards-top-mid-img">
+                                        <img className='w-100' src= {a.img} alt="" />
                                     </div>
-                                    <h3 className='mt-2 mb-3'><a href="">Created face stars sixth forth fow Earth firmament meat</a></h3>
-                                    <p><span className='currentDate'>March 12 , 2019</span><span className='text-light'> .By Alen Mark</span></p>
+                                    <div className="cards-top-mid-text">
+                                        <div className="sub-heading">
+                                            <a href="">{a.title}</a>
+                                        </div>
+                                        <h3 className='mt-2 mb-3'><a href="">{a.disc}</a></h3>
+                                        <p><span className='currentDate'>March 12 , 2019</span><span className='text-light'> .By Alen Mark</span></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        ))}
+
                         <div className="col-lg-3 col-md-12 col-xl-3">
+                        
                             <div className="row g-4">
+                            {News.slice(2, 4).map((a) => (
                                 <div className="col-lg-12 col-md-6">
                                     <div className="cards-top-right">
                                         <div className="cards-top-right-img">
-                                            <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/3.jpg" alt="" />
+                                            <img className='w-100' src={a.img} alt="" />
                                         </div>
                                         <div className="cards-top-right-text text-center">
                                             <div className="sub-heading">
-                                                <a className='' href="">Lifestyle</a>
+                                                <a className='' href="">{a.title}</a>
                                             </div>
-                                            <h6><a href="">The abundantly brought after day fish there image</a></h6>
+                                            <h6><a href="">{a.disc}</a></h6>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-lg-12 col-md-6">
-                                    <div className="cards-top-right">
-                                        <div className="cards-top-right-img">
-                                            <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/4.jpg" alt="" />
+                                ))}
+                            </div>
+                        
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+            <div className="cards-bottom">
+                <div className="container">
+                    <div className="row g-4">
+
+                        <div className="col-lg-6  col-md-12">
+                            <div className="cards-bottom-left">
+                                <div className="row">
+                                    <div className="col-md-5 col-sm-5">
+                                        <div className="cards-bottom-left-img">
+                                            <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/5.jpg" alt="" />
+
                                         </div>
-                                        <div className="cards-top-right-text text-center">
+                                    </div>
+                                    <div className="col-md-7 col-sm-7">
+                                        <div className="cards-bottom-left-text ">
                                             <div className="sub-heading">
-                                                <a className='' href="">Lifestyle</a>
+                                                <a className='' href="">TOURS & TRAVELS</a>
                                             </div>
-                                            <h6><a href="">The abundantly brought after day fish there image</a></h6>
+                                            <h3 className='mt-2 mb-3'><a href="">Brought all day domi nion appear from subdue dominion firmament over face</a></h3>
+                                            <p><span className='currentDate'>March 12 , 2019</span><span className='text-dark'> .By Alen Mark</span></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="cards-bottom">
-                    <div className="container">
-                        <div className="row g-4">
-
-                            <div className="col-lg-6  col-md-12">
-                                <div className="cards-bottom-left">
-                                    <div className="row">
-                                        <div className="col-md-5 col-sm-5">
-                                            <div className="cards-bottom-left-img">
-                                                <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/5.jpg" alt="" />
-
-                                            </div>
-                                        </div>
-                                        <div className="col-md-7 col-sm-7">
-                                            <div className="cards-bottom-left-text ">
-                                                <div className="sub-heading">
-                                                    <a className='' href="">TOURS & TRAVELS</a>
-                                                </div>
-                                                <h3 className='mt-2 mb-3'><a href="">Brought all day domi nion appear from subdue dominion firmament over face</a></h3>
-                                                <p><span className='currentDate'>March 12 , 2019</span><span className='text-dark'> .By Alen Mark</span></p>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="col-lg-3 col-md-6">
+                            <div className="cards-bottom-right">
+                                <div className="cards-bottom-right-img">
+                                    <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/6.jpg" alt="" />
+                                </div>
+                                <div className="cards-bottom-right-text">
+                                    <a href="">Tours & Travel</a>
+                                    <h6 className='mb-3'><a href="">Abundantly forth late appear fourth us.</a></h6>
                                 </div>
                             </div>
-                            <div className="col-lg-3 col-md-6">
-                                <div className="cards-bottom-right">
-                                    <div className="cards-bottom-right-img">
-                                        <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/6.jpg" alt="" />
-                                    </div>
-                                    <div className="cards-bottom-right-text">
-                                        <a href="">Tours & Travel</a>
-                                        <h6 className='mb-3'><a href="">Abundantly forth late appear fourth us.</a></h6>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className="col-lg-3 col-md-6">
-                                <div className="cards-bottom-right">
-                                    <div className="cards-bottom-right-img">
-                                        <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/7.jpg" alt="" />
-                                    </div>
-                                    <div className="cards-bottom-right-text">
-                                        <a href="">Tours & Travel</a>
-                                        <h6 className='mb-3'><a href="">Abundantly forth late appear fourth us.</a></h6>
-                                    </div>
-                                </div>
-
-                            </div>
                         </div>
+                        <div className="col-lg-3 col-md-6">
+                            <div className="cards-bottom-right">
+                                <div className="cards-bottom-right-img">
+                                    <img className='w-100' src="https://preview.colorlib.com/theme/eden/img/magazine/7.jpg" alt="" />
+                                </div>
+                                <div className="cards-bottom-right-text">
+                                    <a href="">Tours & Travel</a>
+                                    <h6 className='mb-3'><a href="">Abundantly forth late appear fourth us.</a></h6>
+                                </div>
+                            </div>
 
-
+                        </div>
                     </div>
-                </div>
 
+
+                </div>
             </div>
         </>
     )
