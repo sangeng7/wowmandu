c8018842a28b449c433e45e49b491da90334d641
sangeng7 <sangeng7@gmail.com>
2024-01-10 15:21:09 +0545

    Footer Updated

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,84 +1,40 @@
 import React from 'react'
 import './footer.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
-import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import {  faLinkedin, faSquareFacebook, faSquareInstagram, faSquareXTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
         <>
             <div className="footer">
                 <div className="container">
-                    <div className="footer-top">
-                        <div className="row g-4">
-                            <div className="footer-1 col-lg-3 col-sm-6 col-md-6">
-                                <div className="footer-top-header">
-                                    <h5 className='mb-4'>About Us</h5>
-                                </div>
-                                <p>Heaven fruitful doesn't over lesser days appear creeping seasons so behold bearing days open</p>
-                                <div className="footer-top-logo">
-                                    <img className='w-100' src="footerLogo.jpg" alt="" />
-                                </div>
-                            </div>
-                            <div className="footer-1 col-lg-3 col-sm-6 col-md-6">
-                                <div className="footer-top-header">
-                                    <h5 className='mb-4'>Contact Info</h5>
-                                </div>
-                                <p className='mb-3'>Address :Your address goes here, your demo address.</p>
-                                <p className='mb-3'>Phone : +8880 44338899 </p>
-                                <p>Email : info@colorlib.com</p>
-
-                            </div>
-                            <div className="footer-1 col-lg-3 col-sm-6 col-md-6">
-                                <div className="footer-top-header">
-                                    <h5 className='mb-4'>Important Links</h5>
-                                </div>
-                                <ul>
-                                    <li className='mb-2'><p>WHMCS-bridge</p></li>
-                                    <li className='mb-2'><p>Search Domain</p></li>
-                                    <li className='mb-2'><p>My Account</p></li>
-                                    <li className='mb-2'><p>Shopping Cart</p></li>
-                                    <li className='mb-2'><p>Our Shop</p></li>
-                                </ul>
-                            </div>
-                            <div className="footer-1 col-lg-3 col-sm-6 col-md-6">
-                                <div className="footer-top-header">
-                                    <h5 className='mb-4'>Send Message</h5>
-                                </div>
-                                <p>Heaven fruitful doesn't over lesser in days. Appear creeping seasons deve behold bearing days open</p>
-                                <div className="textarea">
-                                    <input type='email' placeholder='Your Email Address' />
-                                    <button type='submit' className="send-icon">
-                                        <FontAwesomeIcon icon={faPaperPlane} />
-                                    </button>
-                                </div>
+                    <div className="row align-items-center">
+                        <div className="col-lg-3 col-sm-12">
+                            <div className="footer-logo pe-3">
+                                <img className='w-100' src="logo.png" alt="" />
                             </div>
                         </div>
-                    </div>
-                    <div className="footer-bottom">
-                        <div className="row">
-                            <div className="col-md-4 col-xs-12 ">
-                                <div className="footer-bottom-left">
-                                    <p>Copyright ©2023 All rights reserved</p>
-                                    
-                                </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="footer-contact text-center pe-3">
+                                <h5><span className='me-2'><FontAwesomeIcon icon={faPhone} /></span>+977 01234444</h5>
+                                <h5><span className='me-2'><FontAwesomeIcon icon={faEnvelope} /></span>wowmandu@gmail.com</h5>
                             </div>
-                            <div className="col-md-4 col-xs-12">
-                                <div className="footer-bottom-right text-center">
-                                    <p className='d-inline-block me-2 fb'><a href="https://www.facebook.com/wowmandu"><FontAwesomeIcon icon={faFacebookF} /></a></p>
-                                    <p className='d-inline-block mx-2 inst'><a href=""><FontAwesomeIcon icon={faInstagram} /></a></p>
-                                    <p className='d-inline-block mx-2 twt'><a href=""><FontAwesomeIcon icon={faXTwitter} /></a></p>
-                                    <p className='d-inline-block ms-2 gg'><a href=""><FontAwesomeIcon icon={faGoogle} /></a></p>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="footer-icon text-center pe-3">
+                                <ul className='d-flex justify-content-around'> 
+                                    <li className='fb'><a href="https://www.facebook.com/wowmandu"><FontAwesomeIcon icon={faSquareFacebook} /></a></li>
+                                    <li className='twt'><a href="https://www.instagram.com/"><FontAwesomeIcon icon={faSquareInstagram} /></a></li>
+                                    <li className='inst'><a href="https://twitter.com/"><FontAwesomeIcon icon={faSquareXTwitter} /></a></li>
+                                    <li className='lkdn'><a href="https://www.linkedin.com/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
 
-                                </div>
+                                </ul>
                             </div>
-                            <div className="col-md-4">
-                                <div className="footer-bottom-mid">
-                                    <p>Designed & Developed By WebEdge Info Tech</p>
-                                </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-12">
+                            <div className="footer-designer text-center">
+                                <h5>Developed & designed by <span className=''><a href="https://webedgeinfotech.com/">WebEdge InfoTech</a></span></h5>
                             </div>
                         </div>
                     </div>
